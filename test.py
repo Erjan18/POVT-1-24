524870100e475b00d70e85514d83ce79250f6f7c
Erjan18 <era.kar02@gmail.com>
2025-02-06 10:18:57 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# onProject/
+#
+# second@Era MINGW64 ~/PycharmProjects/PythonProject
+# $ ls
+# bot.py  image/
+#
+# second@Era MINGW64 ~/PycharmProjects/PythonProject
+# $ git clone https://github.com/Erjan18/POVT-1-24.git
+# Cloning into 'POVT-1-24'...
+# remote: Enumerating objects: 4, done.
+# remote: Counting objects: 100% (4/4), done.
+# remote: Compressing objects: 100% (3/3), done.
+# remote: Total 4 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+# Receiving objects: 100% (4/4), done.
+#
+# second@Era MINGW64 ~/PycharmProjects/PythonProject
+# $ ls
+# POVT-1-24/  bot.py  image/
+#
